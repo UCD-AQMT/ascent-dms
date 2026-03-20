83af10a0a9636e0be350e3e4226b183f3baa05d5
sraffuse <sraffuse@ucdavis.edu>
2026-03-20 16:12:24 -0700

    WIP on ae33 L2

--- services/r/ascentr/R/ae33.R
+++ services/r/ascentr/R/ae33.R
@@ -463,15 +463,17 @@ ae33_l2_from_files <- function(l1b_file, manual_qc_file) {
   qc <- readr::read_csv(manual_qc_file)
 
   qc <- qc |>
+    mutate(flag = as.character(flag)) |>
     rename(manual_flag=flag, manual_comment=comment)
 
   df <- l1b |>
     left_join(qc, by = join_by(between(sample_datetime_UTC,
                                        sample_datetime_UTC_start,
                                        sample_datetime_UTC_end))) |>
-    left_join(available_flags, by = "manual_flag")
+    left_join(available_flags, by = "manual_flag") |>
+    mutate(flag = as.character(flag))
 
-  # Coalesce flags and comments
+  # Coalesce flags and comments and calculate the base hour
   df <- df |>
     mutate(final_flag = if_else(is.na(manual_flag), flag,
                                 if_else(is.na(flag), manual_flag,
@@ -480,11 +482,118 @@ ae33_l2_from_files <- function(l1b_file, manual_qc_file) {
                                       pmax(qc_outcome, manual_qc_outcome)),
            final_comment = if_else(is.na(manual_comment), comment,
                                    if_else(is.na(comment), manual_comment,
-                                           paste(manual_comment, comment, sep = " : "))))
+                                           paste(manual_comment, comment, sep = " : ")))) |>
+    mutate(sample_hour_UTC = lubridate::floor_date(sample_datetime_UTC, "1 hour"),
+           .after = site_code)
 
   df <- df |>
     select(!(flag:manual_qc_outcome)) |>
     rename(flag=final_flag, qc_outcome=final_qc_outcome, comment=final_comment)
+  
+  # To properly calculate BB% at hourly resolution, we convert the 1-minute data to
+  # concentration, average that to 1hr, then convert back.
+  # BB% is calculated using 950nm (channel 7)
+  
+  ###### TESTING BB Math  --- This is not currently correct
+  
+  mac_470 <- 14.54
+  mac_950 <- 7.19
+  ang_ff <- 1
+  ang_bb <- 2
+  
+  bb_calcs <- df |>
+    select(sample_datetime_UTC, bc_2_STP_ng_m3, bc_6_STP_ng_m3, bc_7_STP_ng_m3, bb_percent) |>
+    mutate(abs_470 = bc_2_STP_ng_m3 * mac_470,
+           abs_950 = bc_7_STP_ng_m3 * mac_950,
+           abs_bb = (abs_470 - abs_950 * (470/950)^-ang_ff) / ((470/950)^-ang_bb - (470/950)^-ang_ff),
+           bc_bb = abs_bb / 7.77,
+           bb_frac = bc_bb / bc_6_STP_ng_m3)
+  
+  
+  
+  #####
+  
+  
+  df <- df |>
+    mutate(bb_conc = bc_7_STP_ng_m3 * bb_percent / 100)
+  
+  
+  # AE33 sampling is every 1 minute - 60 samples per hour
+  # Require 30 samples for a valid hourly measurement
+  samples_required <- 30
+  hourly_counts <- df |>
+    mutate(valid = if_else(qc_outcome < 4, "valid", "invalid")) |>
+    summarise(count = n(),
+              .by = c(sample_hour_UTC, valid)) |>
+    tidyr::pivot_wider(names_from = valid, values_from = count)
+  
+  valid_hours <- hourly_counts |>
+    filter(valid >= samples_required)
+  invalid_hours <- setdiff(hourly_counts, valid_hours)
 
+  # Process hourly results for valid samples
+  data_hourly_valid <- df |>
+    filter(sample_hour_UTC %in% valid_hours$sample_hour_UTC) |> # only valid hours
+    filter(qc_outcome < 4) |> # within those hours only process valid observations
+    summarise(across(starts_with("bc"), ~mean(.x, na.rm = TRUE)),
+              across(starts_with("k_"), ~mean(.x, na.rm = TRUE)),
+              across(starts_with("att"), ~mean(.x, na.rm = TRUE)),
+              bb_conc = mean(bb_conc, na.rm = TRUE),
+              .by = sample_hour_UTC) |>
+    mutate(bb_percent = bb_conc / bc_7_STP_ng_m3 * 100) |>
+    select(-bb_conc)
+  
+  # rejoin with flags and other info
+  flags_hourly_valid <- df |>
+    filter(sample_hour_UTC %in% valid_hours$sample_hour_UTC) |>
+    filter(qc_outcome < 4) |>
+    select(site_number, site_code, sample_hour_UTC, qc_outcome, flag, comment) |>
+    summarise(site_number = first(site_number),
+              site_code = first(site_code),
+              qc_outcome = max(qc_outcome),
+              flag = recompose_flags(flag),
+              comment =recompose_flags(comment),
+              .by = sample_hour_UTC)
+  
+  df_valid <- flags_hourly_valid |>
+    left_join(select(valid_hours, sample_hour_UTC, sample_count=valid), 
+              by = "sample_hour_UTC") |>
+    left_join(data_hourly_valid, by = "sample_hour_UTC")
+  
+  # Get the flags and associated data for the invalid time periods, which will be filled
+  # with nulls
+  # Some may be invalid because not enough samples but no bad qc_outcome. If so, downgrade
+  flags_hourly_invalid <- df |>
+    filter(sample_hour_UTC %in% invalid_hours$sample_hour_UTC) |>
+    select(site_number, site_code, sample_hour_UTC, qc_outcome, flag, comment) |>
+    summarise(site_number = first(site_number),
+              site_code = first(site_code),
+              qc_outcome = max(qc_outcome),
+              flag = recompose_flags(flag),
+              comment = recompose_flags(comment),
+              .by = sample_hour_UTC) |>
+    left_join(select(invalid_hours, sample_hour_UTC, sample_count=valid)) |>
+    mutate(flag = if_else(qc_outcome < 4, "391", flag),
+           comment = if_else(qc_outcome < 4, "391-Data completeness less than 50%", comment),
+           qc_outcome = if_else(qc_outcome < 4, 9, qc_outcome))
+  
+  if (nrow(flags_hourly_invalid) > 0) {
+    result <- bind_rows(df_valid, flags_hourly_invalid) |>
+      arrange(sample_hour_UTC) |>
+      rename(sample_datetime_UTC=sample_hour_UTC)  
+  } else {
+    result <- df_valid |>
+      arrange(sample_hour_UTC) |>
+      rename(sample_datetime_UTC=sample_hour_UTC) 
+  }
+  
+  # Rearrange to final field order
+  result <- result |>
+    select(sample_datetime_UTC, site_number, site_code, sample_count:bb_percent,
+           qc_outcome, flag, comment) |>
+    mutate()
+
+  return(result)
+  
 }
 
