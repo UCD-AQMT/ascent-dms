af5b45bcea26e0a2dab87934f1989bd1920195d7
sraffuse <sraffuse@ucdavis.edu>
2026-05-04 12:01:21 -0700

    WIP: improve download page

--- services/r/ascentr/R/export.R
+++ services/r/ascentr/R/export.R
@@ -10,7 +10,7 @@ export_csv <- function(df, outfile) {
 }
 
 export_zip <- function(df, meta, folder, fname) {
- 
+
   temp_dir <- file.path(tempdir(), as.integer(Sys.time()))
   dir.create(temp_dir)
   txt_file <- file.path(temp_dir, paste0(fname, ".txt"))
@@ -22,6 +22,24 @@ export_zip <- function(df, meta, folder, fname) {
   zip_file <- file.path(folder, paste0(fname, ".zip"))
   zip::zip(zipfile = zip_file, files = c(txt_file, csv_file), mode = "cherry-pick")
    
+}
+
+export_zip_shiny <- function(df, meta, fname, temp_file) {
+  
+  temp_dir <- file.path(tempdir(), as.integer(Sys.time()))
+  dir.create(temp_dir)
+  txt_file <- file.path(temp_dir, paste0(fname, ".txt"))
+  writeLines(meta, txt_file)
+  csv_file <- file.path(temp_dir, paste0(fname, ".csv"))
+  # Convert all POSIX to formatted strings
+  df <- format_dates(df)
+  write.csv(df, csv_file, na = "", row.names = FALSE)
+  zip::zip(
+    zipfile = temp_file,
+    files = c(txt_file, csv_file),
+    mode = "cherry-pick"
+  )
+  
 }
 
 format_dates <- function(df) {
