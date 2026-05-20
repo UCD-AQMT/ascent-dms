bd2e3cecc3bae5af56f7e790e73aabed67906196
sraffuse <sraffuse@ucdavis.edu>
2026-05-20 12:07:59 -0700

    Add monthly l1b export function

--- services/r/ascentr/R/export.R
+++ services/r/ascentr/R/export.R
@@ -1,6 +1,69 @@
 
 # Some functions for exporting delivery files, ensuring proper formatting
 
+#' Title
+#'
+#' @param site 
+#' @param month_date 
+#' @param instrument 
+#' @param folder 
+#' @param con 
+#' @param influx_con 
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+monthly_l1b <- function(site, month_date, instrument, folder, con, influx_con = NULL) {
+  
+  end_date <- lubridate::rollforward(as.Date(month_date))
+  
+  fname <- paste("ASCENT", instrument, site, strftime(month_date, format = "%Y%m%d"),
+                 strftime(end_date, format = "%Y%m%d"), "L1b", sep = "_")
+  
+  if (instrument == "AE33") {
+    if (is.null(influx_con)) {
+      stop("influx_con must be provided to process AE33")
+    }
+    df <- ae33_l1b(site, month_date, end_date, influx_con)
+    if (is.null(df)) {
+      warning("No AE33 data for ", site, " ", month_date, " - ", end_date)
+      return(NULL)
+    }
+    meta <- ae33_metadata(site, month_date, end_date, level = "1b", con = con)
+  } else if (instrument == "SMPS") {
+    df <- smps_l1b_df(site, month_date, end_date, con)
+    if (is.null(df)) {
+      warning("No SMPS data for ", site, " ", month_date, " - ", end_date)
+      return(NULL)
+    }
+    meta <- smps_metadata(site, month_date, end_date, level = "1b", con = con)
+  } else if (instrument == "Xact") {
+    df <- xact_l1b(site, month_date, end_date, con)
+    if (is.null(df)) {
+      warning("No Xact data for ", site, " ", month_date, " - ", end_date)
+      return(NULL)
+    }
+    meta <- xact_metadata(site, month_date, end_date, level = "1b", con = con)
+  } else if (instrument == "ACSM") {
+    result <- acsm_l1b(site, month_date, end_date, con)
+    df <- result$df
+    mdf <- result$mdf
+    if (is.null(df)) {
+      warning("No Xact data for ", site, " ", month_date, " - ", end_date)
+      return(NULL)
+    }
+    meta <- acsm_metadata(site, month_date, end_date, level = "1b", con = con, metadata_fields = mdf)
+  } else {
+    stop("Cannot export instrument named ", instrument)
+  }
+  
+  
+  export_zip(df, meta, folder, fname)
+  
+}
+
+
 export_csv <- function(df, outfile) {
   
   # Convert all POSIX to formatted strings
