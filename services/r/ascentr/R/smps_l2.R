9fca65938d93c6d613b3435875ef4f570ca51cdf
sraffuse <sraffuse@ucdavis.edu>
2026-08-31 12:56:22 -0700

    Add guard for no data

--- services/r/ascentr/R/smps_l2.R
+++ services/r/ascentr/R/smps_l2.R
@@ -21,6 +21,10 @@ smps_l2_prepare_df <- function(l1b_file, manual_qc_file, start_datetime = NULL) 
     distinct()
 
   l1b <- readr::read_csv(l1b_file, show_col_types = FALSE, guess_max = 50000)
+  if (nrow(l1b) == 0) {
+    stop("No data in ", l1b_file)
+  }
+
   qc <- readr::read_csv(manual_qc_file, col_types = "TTcc")
 
   prb <- vroom::problems(qc)
