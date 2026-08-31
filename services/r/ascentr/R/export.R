7c9b2f9e9c87a83d6dcb81f85affd339e931be25
sraffuse <sraffuse@ucdavis.edu>
2026-08-31 12:57:25 -0700

    Add export_ae33_l2_from_l1b

--- services/r/ascentr/R/export.R
+++ services/r/ascentr/R/export.R
@@ -82,6 +82,29 @@ export_smps_l2_from_l1b <- function(l1b_zip, start_date, end_date, site, manual_
 
 }
 
+export_ae33_l2_from_l1b <- function(l1b_zip, start_date, end_date, site, manual_qc_file, 
+                                    level = "2", out_folder, con) {
+  
+  # Open L1b zip file
+  Sys.sleep(1)
+  temp_dir <- file.path(tempdir(), as.integer(Sys.time()))
+  dir.create(temp_dir)
+  zipfile <- unzip(l1b_zip, exdir = temp_dir)
+  l1b_csv <- fs::dir_ls(temp_dir, glob = "*.csv")
+  
+  # Pass csv to ae33_l2_from_files
+  if (level == "2") {
+    df <- ae33_l2_from_files(l1b_csv, manual_qc_file, start_datetime = start_date)
+    m <- ae33_metadata(site, start_date, end_date, con = con, level = "2")
+    fname <- glue::glue("ASCENT_AE33_{site}_{start_date}_{end_date}_L2")
+  } else if (level == "2N") {
+    df <- ae33_l2_native_from_files(l1b_csv, manual_qc_file, start_datetime = start_date)
+    m <- ae33_metadata(site, start_date, end_date, con = con, level = "2N")
+    fname <- glue::glue("ASCENT_AE33_{site}_{start_date}_{end_date}_L2_native")
+  }
+  export_zip(df, m, out_folder, fname)
+  
+}
 
 #' Title
 #'
